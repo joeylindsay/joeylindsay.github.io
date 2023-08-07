d366f308771cb887ad5a937ea639ba7ff1df1053
Joey Lindsay <jlindsay@esri.com>
2023-08-07 12:56:50 -0400

    blog 8/6/2023

---
+++ app/Writing/Blog862023.tsx
@@ -0,0 +1,138 @@
+import { List, ListItemText, Typography } from '@mui/material';
+
+export const Blog862023 = (
+  <Typography
+    variant="body1"
+    sx={{ fontSize: 14, p: 2 }}
+    lineHeight="2"
+    component="div"
+  >
+    <p>I’m still waiting for Kohaku.</p>
+    <p>
+      Hayao Miyazaki is a lover of nature. The fantastic jungles and razed
+      landscapes of <i>Nausicaä of the Valley of the Wind</i> were inspired by
+      the real-world poisoning of Minamata Bay. <i>Princess Mononoke</i> depicts
+      a clash between humans and the forces of the wild, which are led by the
+      titular princess from the back of a great white wolf. The forest from the
+      film is drawn directly from the wilderness core of Yakushima Island, a
+      UNESCO World Heritage site. <i>My Neighbor Totoro</i> has been described
+      by one of its producers as &quot;nature painted with translucent
+      colors.&quot;
+      <sup>[1]</sup>
+    </p>
+    <p>
+      But those aren’t the Miyazaki movies I’ll be discussing this week, because
+      today I happened to rewatch <i>Spirited Away</i>. I must have seen this
+      movie more times than there are stars in the sky, a feat that becomes
+      easier with each passing day, as increasing light pollution obscures more
+      of the heavens. <i>Spirited Away</i> similarly grapples with
+      environmentalism, although unlike those earlier films, the theme watches
+      serenely from the shadows rather than bombastically crashing through
+      Yubaba’s bathhouse doors. It flickers briefly into view when Chihiro and
+      the bathhouse employees clean what, at first glance, seems to be a stink
+      spirit. However, after a thorough scrubbing, an old river spirit emerges
+      from underneath the pile of filth, and thanks its cleanup crew with gifts
+      of medicine and gold dust. It appears once again later in the story, when
+      Chihiro reveals that her companion Haku is actually the spirit of the dead
+      Kohaku river, which was infilled and covered with apartment blocks.
+    </p>
+    <p>
+      A few weeks ago, I attempted to take our family dog for a walk along
+      Difficult Run, a stream in Northern Virginia. The banks have become so
+      eroded that when she tried to jump down them, she fell further than
+      expected and twisted her paw. I had to carry her home. My family in
+      Florida often shake their heads at America’s treatment of the Kissimmee
+      River, which was disastrously straightened by the US Army Corps of
+      Engineers between 1962 and 1970. This dried swaths of wetlands, caused
+      runaway pollution buildup, decimated native species, and destroyed the
+      water quality of central Florida. Nook Farm is a neighborhood in
+      Connecticut, so named for a nearby nook in the Park River. In the late
+      1800s, the area attracted an artist community that included Mark Twain,
+      who enjoyed seeing its marshes and wildlife from his house.<sup>[2]</sup>{' '}
+      While Twain’s home is listed on the National Register of Historic Places,
+      in a cruel twist, the eponymous nook was filled to create a parking lot
+      for tourists visiting his former residence. Eventually, the Army Corps of
+      Engineers buried the entire Park River underneath Hartford. Our desecrated
+      waterways are even displayed prominently in cinema. Dozens of movies,
+      including <i>Grease</i> and <i>Terminator 2</i>, feature scenes set in the
+      concrete channels of the Los Angeles River. It was once a high-volume
+      waterway with a staggering variety of native wildlife, until the US Army
+      Corps of Engineers (I’m sensing a pattern here) blocked most of its flow
+      and turned the entire river into a trapezoidal drainage ditch. Fantastic
+      for filming car chases, terrible for most anything else.
+    </p>
+    <p>
+      These activities are hardly limited to the United States – it is a global
+      issue. Earlier this year, photographs of the Tietê River in Brazil
+      circulated the internet. Its surface is currently covered in a toxic foam
+      brought on by industrial runoff. The frothing white mass poisons any soil,
+      plants, animals, or people it comes into contact with. The Ganges River is
+      infamously one of the most unsafe holy sites in the world, sporting
+      horrifically unsafe levels of fecal coliform, as well as a baker’s dozen
+      ecological dead zones caused by agricultural runoff.
+    </p>
+    <p>
+      These terrifying tales of wanton misuse of our natural resources send me
+      scurrying back across the threshold of the spirit world. When the filthy
+      river spirit enters the bathhouse, <i>everyone</i> pitches in to bathe
+      him. Yubaba, the film’s primary antagonist, realizes the benefits of
+      cleaning the river, and helps direct the entire operation. She is rewarded
+      with literal piles of gold. Back in reality, the same lesson applies –
+      taking care of our watersheds helps <i>everybody</i>, from international
+      megacorporations to small local restaurants, from jet-setting billionaires
+      to urban homeless.
+    </p>
+    <p>
+      This realization perhaps struck no organization more completely than the
+      US Army Corps of Engineers. They are no longer the shortsighted agency
+      that authored those destructive megaprojects in the 1960s and 1970s. They
+      now pursue comprehensive management programs that balance the competing
+      needs of safety, environment, and water security. Under the Estuary
+      Restoration Act, they have led dozens of projects to restore and protect
+      critical US waterways. They even repaired the damage they caused in the
+      Everglades, putting bends back into the Kissimmee river, demolishing the
+      retention ponds they constructed, and backfilling the central canal. These
+      efforts were completed in 2021, restoring 20,000 acres of habitat.
+      Wildlife populations are already rebounding.
+    </p>
+    <p>
+      Cities are no longer burying their rivers, either. Instead, “daylighting”
+      has become a trend in places like New York, which recently re-exposed the
+      Saw Mill River to the surface. This action stimulated real estate
+      development, created new parks for Yonkers residents, and improved flood
+      resiliency, since runoff is no longer confined to a fixed-diameter
+      culvert. As larger and more frequent rain storms begin to pound the
+      northeast, such projects will become critical components of modern urban
+      design.
+    </p>
+    <p>
+      But we are still falling woefully short. When will our leaders fully
+      recognize that our water resources are among the most valuable assets
+      provided to us by nature? The filtering and distribution of fresh water is
+      a multi-trillion dollar industry that the planet performs for us at
+      absolutely zero cost. I’m not asking CEOs and politicians to become
+      Princess Mononoke, and ride into battle against industrialization on the
+      back of a bloodthirsty wolf. I just want them to act like Yubaba, who
+      knows that at the bottom of a clean river lies untold riches.
+    </p>
+    <p>
+      At the conclusion of <i>Spirited Away</i>, Kohaku tells Chihiro that he
+      will see her again someday. Maybe we will daylight the Kohaku River. Then,
+      he will be able to return to his old home in the real world, and see
+      Chihiro’s reflection smiling in his glimmering surface. Or maybe the two
+      will be kept apart until she passes back into the spirit world for the
+      last time, and joins the shapeless ghosts lingering outside the bathhouse.
+      It’s an ending that hasn’t been written yet, and I know which one I want
+      to see. So for now, I’ll keep waiting for Kohaku to return.
+    </p>
+    <List>
+      <ListItemText>
+        [1] Kikuchi, Yoshiaki (August 4, 2007). &quot;Totoro&apos;s set
+        decorator&quot;. Daily Yomiuri.
+      </ListItemText>
+      <ListItemText>
+        [2] The Loveliest Home That Ever Was. Mark Twain House.
+      </ListItemText>
+    </List>
+  </Typography>
+);
--- app/Writing/WritingPage.tsx
+++ app/Writing/WritingPage.tsx
@@ -2,6 +2,7 @@ import { Stack } from '@mui/material';
 import { OpenLetter572023 } from './OpenLetter572023';
 import { WritingEntry } from './WritingEntry';
 import { OpenLetter7302023 } from './OpenLetter7302023';
+import { Blog862023 } from './Blog862023';
 
 export const WritingPage = (): JSX.Element => (
   <Stack
@@ -19,6 +20,9 @@ export const WritingPage = (): JSX.Element => (
     </WritingEntry>
     <WritingEntry name="An Open Letter to President Biden - 5/7/2023">
       {OpenLetter572023}
+    </WritingEntry>
+    <WritingEntry name="Waiting for Kohaku - 8/6/2023">
+      {Blog862023}
     </WritingEntry>
   </Stack>
 );
